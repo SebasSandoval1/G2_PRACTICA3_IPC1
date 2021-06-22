6fbd5d331c52eee9740db944a16802cd80341ce3
Ahletz <85268562+Ahletz@users.noreply.github.com>
2021-06-21 23:40:19 -0600

    uwu

--- src/PRAC3/Main.java
+++ src/PRAC3/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Imaginate que funcione xd");
+        System.out.println("a ver si el mio funciona :c");
 
     }
 }
