591b70993df5a681520cb215475b3d4e437947c0
Ahletz <85268562+Ahletz@users.noreply.github.com>
2021-06-26 04:33:46 -0600

    ORDENAMIENTO QUICKSORT COMPLETO

--- src/PRAC3/OrdenamientoQ.java
+++ src/PRAC3/OrdenamientoQ.java
@@ -3,7 +3,30 @@ package PRAC3;
 public class OrdenamientoQ {
 
 //arreglo auxiliar
+Asignaciones [] aux = new Asignaciones[1];
 
+public void MetodoQk(){
+
+    for (int i = 0; i < CargaDeDatos.ArrayAsignaciones.length-1; i++) {
+
+        for (int j = 1; j < CargaDeDatos.ArrayAsignaciones.length-1; j++) {
+
+            if (CargaDeDatos.ArrayAsignaciones[j].Nota<CargaDeDatos.ArrayAsignaciones[j-1].Nota){
+
+                aux[0] =CargaDeDatos.ArrayAsignaciones[j];
+                CargaDeDatos.ArrayAsignaciones[j] =CargaDeDatos.ArrayAsignaciones[j-1];
+                CargaDeDatos.ArrayAsignaciones[j-1]=aux[0];
+            }
+            if (CargaDeDatos.ArrayAsignaciones[j].Nota>CargaDeDatos.ArrayAsignaciones[j+1].Nota){}
+            aux[0] =CargaDeDatos.ArrayAsignaciones[j];
+            CargaDeDatos.ArrayAsignaciones[j] =CargaDeDatos.ArrayAsignaciones[j+1];
+            CargaDeDatos.ArrayAsignaciones[j+1]=aux[0];
+        }
+
+    }
+
+
+}
 
 
 
